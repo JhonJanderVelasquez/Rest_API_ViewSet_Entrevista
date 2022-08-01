db4a3d54b67e3f19e8a0b0278a6d12dbfcfaf1c0
JhonJanderVelasquez <jhonspeinx@gmail.com>
2022-07-31 20:23:07 -0500

    Configurado Admin del Modelo.

--- API_Rest_Zaito/API/admin.py
+++ API_Rest_Zaito/API/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from API.models import *
 
-# Register your models here.
+class ImageAdmin (admin.ModelAdmin):
+    list_display = ('id', 'producto')
+
+class ProductAdmin (admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'precio')
+
+admin.site.register(Producto, ProductAdmin)
+admin.site.register(Imagen, ImageAdmin)
